d23424f292f9a6a97583f0c0a8acd24695bc5d73
Owen <owenouiluobe@gmail.com>
2024-07-12 16:08:20 +0200

    Modification apporté pour pouvoir faire un npm run start

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,42 +1,12 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import * as mongoose from 'mongoose';
-import { Task } from './schemas/task.schema';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TasksService } from './tasks.service';
+import { TasksController } from './tasks.controller';
+import { TaskSchema } from 'src/tasks/schemas/task.schema';
 
-@Injectable()
-export class TasksService {
-  constructor(
-    @InjectModel(Task.name)
-    private taskModel: mongoose.Model<Task>,
-  ) {}
-
-  async findAll(): Promise<Task[]> {
-    const tasks = await this.taskModel.find();
-    return tasks;
-  }
-
-  async create(task: Task): Promise<Task> {
-    const res = await this.taskModel.create(task);
-    return res;
-  }
-
-  async findById(id: string): Promise<Task> {
-    const task = await this.taskModel.findById(id);
-
-    if (!task) {
-      throw new NotFoundException('Task not found.');
-    }
-
-    return task;
-  }
-
-  async updateById(id: string, task: Task): Promise<Task> {
-    return await this.taskModel.findByIdAndUpdate(id, task, {
-      new: true,
-    });
-  }
-
-  async deleteById(id: string): Promise<Task> {
-    return await this.taskModel.findByIdAndDelete(id);
-  }
-}
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'Task', schema: TaskSchema }])],
+  providers: [TasksService],
+  controllers: [TasksController],
+})
+export class TasksModule {}
